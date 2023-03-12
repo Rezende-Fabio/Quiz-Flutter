abf0baebad2ac3b899dc7325b4fa41fe5cabd3be
Rezende-Fabio <rezendefabio337@gmail.com>
2023-03-12 13:26:43 -0300

    create statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
   void responder() {
-    print("Resposta 1 foi lecionada!");
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
     final perguntas = [
       "Qual a sua cor favorita?",
-      "Qual o seu animal favorito?"
+      "Qual o seu animal favorito?",
     ];
 
     return MaterialApp(
@@ -21,24 +26,29 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(perguntas[0]),
+            Text(perguntas[perguntaSelecionada]),
             ElevatedButton(
               child: Text("Resposta 1"),
               onPressed: responder,
             ),
             ElevatedButton(
               child: Text("Resposta 2"),
-              onPressed: () => print("Resposta 2 foi leceionada"),
+              onPressed: responder,
             ),
             ElevatedButton(
               child: Text("Resposta 3"),
-              onPressed: () {
-                print("Resposta 3 foi leceionada");
-              },
+              onPressed: responder,
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
