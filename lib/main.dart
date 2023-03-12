de6b5f86b1896bcbd408edf10c071ba043489592
Rezende-Fabio <rezendefabio337@gmail.com>
2023-03-12 12:51:33 -0300

    functions in button

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(const PerguntaApp());
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({Key? key}) : super(key: key);
+  void responder() {
+    print("Resposta 1 foi lecionada!");
+  }
 
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      "Qual a sua cor favorita?",
+      "Qual o seu animal favorito?"
+    ];
+
     return MaterialApp(
-      home: Text("Olá Flutter"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas"),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text("Resposta 1"),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text("Resposta 2"),
+              onPressed: () => print("Resposta 2 foi leceionada"),
+            ),
+            ElevatedButton(
+              child: Text("Resposta 3"),
+              onPressed: () {
+                print("Resposta 3 foi leceionada");
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
