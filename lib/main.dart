94781af43a62efb4ca8c4f5d7489b392bd8d2cb9
Rezende-Fabio <rezendefabio337@gmail.com>
2023-03-18 12:21:47 -0300

    adding responses dynamically using for

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
+    List<Widget> respostas = [];
+
+    for (String textoResp in perguntas[_perguntaSelecionada].cast()['resposta']) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -38,9 +44,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]["texto"].toString()),
-            Resposta("Resposta 1", _responder),
-            Resposta("Resposta 2", _responder),
-            Resposta("Resposta 3", _responder),
+            ...respostas,
           ],
         ),
       ),
